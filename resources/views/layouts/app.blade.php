87f692c9171ea6f1936f3357d6356be8d9a5fd4f
Paulo Henrique <paulohenriquedeoliveira2006@gmail.com>
2024-06-19 21:47:18 -0300

    UPDATE 2 	modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,6 +87,87 @@
             background-color: #c82333;
             border-color: #c82333;
         }
+
+
+        /* isso é um teste - caso paragjkf
+         body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
+
+        .container {
+            background-color: #fff;
+            padding: 30px;
+            margin-top: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        .btn{
+            background-color: #007bff;
+            border-color: #007bff;
+            color: #fff;
+            padding: 10px;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .btn:hover {
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
+
+        .btn-aviso {
+            background-color: #ffc107;
+            border-color: #ffc107;
+            color: #fff;
+            padding: 10px;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .btn-aviso:hover {
+            background-color: #e0a800;
+            border-color: #e0a800;
+        }
+
+        .btn-perigo {
+            background-color: #dc3545;
+            border-color: #dc3545;
+            color: #fff;
+            padding: 10px;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .btn-perigo:hover {
+            background-color: #c82333;
+            border-color: #c82333;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+        */
     </style>
 </head>
 <body>
